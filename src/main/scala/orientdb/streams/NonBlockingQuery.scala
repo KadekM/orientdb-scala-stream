d7403f84dfd98dd856957070fff6c8e2f70ac1a1
Marek Kadek <marek@spring.co.nz>
2015-07-01 15:31:03 +1200

    Minor cleanup

--- src/main/scala/orientdb/streams/NonBlockingQuery.scala
+++ src/main/scala/orientdb/streams/NonBlockingQuery.scala
@@ -2,7 +2,6 @@ package orientdb.streams
 
 import akka.actor.ActorSystem
 import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx
-import com.orientechnologies.orient.core.sql.query.OSQLNonBlockingQuery
 import org.reactivestreams.Publisher
 import orientdb.streams.impl._
 
@@ -20,8 +19,4 @@ object NonBlockingQuery {
                args: Map[Object, Object] = Map.empty[Object, Object])
               (implicit system: ActorSystem)  =
     new NonBlockingQueryImpl[A](query, limit, fetchPlan, args)
-/*
-  // throws away listener
-  def apply[A](cmd: OSQLNonBlockingQuery[A])(implicit system: ActorSystem): Publisher[A] =
-    new AsyncQueryImpl[A](cmd.getText, cmd.getLimit, cmd.getFetchPlan, cmd.getParameter)*/
 }
