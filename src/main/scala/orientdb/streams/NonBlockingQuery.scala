35a73d74e60308e2c0f48b002b102af7d0707356
Marek Kadek <marek@spring.co.nz>
2015-07-07 17:03:38 +1200

    Some documentation

--- src/main/scala/orientdb/streams/NonBlockingQuery.scala
+++ src/main/scala/orientdb/streams/NonBlockingQuery.scala
@@ -8,25 +8,42 @@ import orientdb.streams.impl._
 import scala.concurrent.ExecutionContext
 import scala.reflect.ClassTag
 
+/*
+ * Query which does not block, based on OrientDb NonBlockingQuery.
+ */
 trait NonBlockingQuery[A] {
   def execute(args: Any*)(implicit db: ODatabaseDocumentTx, system: ActorSystem, ec: ExecutionContext): Publisher[A]
 }
 
+/*
+ * Query based on OrientDb NonBlockingQuery
+ * Query does not block database processing, but buffers database results on separate thread, and emits
+ * them to downstream if there is demand.
+ *
+ * If downstream cancels, no more elements will be emitted, but database thread will finish it's run (doing the full
+ * fetch, which will though not be buffered and emitted anymore)
+ *
+ * If actor's buffer is too large, it will explode (TODO)
+ */
 object NonBlockingQueryBuffering {
   def apply[A: ClassTag](query: String,
-               limit: Int = -1,
-               fetchPlan: String = null,
-               args: Map[Object, Object] = Map.empty[Object, Object])
-              (implicit system: ActorSystem)  =
-  new NonBlockingQueryBuffering[A](query, limit, fetchPlan, args)
+    limit: Int = -1,
+    fetchPlan: String = null,
+    args: Map[Object, Object] = Map.empty[Object, Object])(implicit system: ActorSystem) =
+    new NonBlockingQueryBuffering[A](query, limit, fetchPlan, args)
 }
 
+/*
+ * Query based on OrientDb NonBlockingQuery
+ * Query blocks database processing unless there is demand. It also BLOCKS one thread, which is doing the
+ * fetch on OrientDb side, so use with care. It may be much slower than using NonBlockingQueryBuffering.
+ *
+ * Has full support for cancellation - when downstream cancels, database will stop the processing.
+ */
 object NonBlockingQueryLocking {
-  // Just experimental, work in progress!
   def apply[A: ClassTag](query: String,
-               limit: Int = -1,
-               fetchPlan: String = null,
-               args: Map[Object, Object] = Map.empty[Object, Object])
-              (implicit system: ActorSystem)  =
-  new NonBlockingQueryLocking[A](query, limit, fetchPlan, args)
+    limit: Int = -1,
+    fetchPlan: String = null,
+    args: Map[Object, Object] = Map.empty[Object, Object])(implicit system: ActorSystem) =
+    new NonBlockingQueryLocking[A](query, limit, fetchPlan, args)
 }
